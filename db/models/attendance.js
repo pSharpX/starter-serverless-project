1b768c1d720227b0827090e1e8373d2e02a5a4f6
pSharpX <guardian2093@gmail.com>
2019-01-19 11:36:34 -0500

    modelo sequelize

--- db/models/attendance.js
+++ db/models/attendance.js
@@ -1,10 +1,52 @@
 module.exports = (sequelize, type) => {
-    return sequelize.define('tag', {
+    return sequelize.define('attendance', {
         id: {
             type: type.INTEGER,
             primaryKey: true,
             autoIncrement: true
         },
-        name: type.STRING
+        action: {
+            type: type.STRING(25),
+            field: 'action_name',
+            allowNull: false
+        },
+        action_enum: {
+            type: type.ENUM,
+            values: ['entrada', 'salida'],
+            field: 'action_enum',
+            allowNull: false
+        },
+        date: {
+            type: type.DATE,
+            field: 'attendance_date',
+            defaultValue: type.NOW,
+            allowNull: false
+        },
+        time: {
+            type: type.TEXT,
+            field: 'time',
+            allowNull: false
+        },
+        employeeId: {
+            type: type.INTEGER,
+            field: 'employee_id',
+            references: {
+                // This is a reference to another model
+                model: Employee,
+                // This is the column name of the referenced model
+                key: 'employeeId'
+                // This declares when to check the foreign key constraint. PostgreSQL only.
+                // deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+            }
+        }
+    }, {
+        freezeTableName: true,
+        tableName: 'attendance',
+        comment: "I'm a table attendance!"
     })
 }
+//
+// employeeId: 0,
+//     action: faker.random.arrayElement(actions),
+//     date,
+//     time: moment(date).format('HH:mm:ss'),
--- db/models/employee.js
+++ db/models/employee.js
@@ -1,10 +1,52 @@
 module.exports = (sequelize, type) => {
-    return sequelize.define('blog', {
+    return sequelize.define('employee', {
         id: {
             type: type.INTEGER,
+            field: 'employee_id',
             primaryKey: true,
             autoIncrement: true
         },
-        text: type.STRING
+        text: type.STRING,
+        firstName: {
+            type: type.STRING,
+            field: 'first_name',
+            allowNull: false
+        },
+        lastName: {
+            type: type.STRING,
+            field: 'last_name',
+            allowNull: false
+        },
+        address: {
+            type: type.TEXT,
+            field: 'address',
+            allowNull: true
+        },
+        email: {
+            type: type.STRING,
+            field: 'email',
+            allowNull: false,
+            unique: true
+        },
+        position: {
+            type: type.STRING,
+            field: 'position',
+            allowNull: false
+        },
+        birthday: {
+            type: type.DATE,
+            field: 'birthday',
+            allowNull: false
+        },
+        dni: {
+            type: type.STRING(20),
+            field: 'dni',
+            allowNull: false,
+            unique: true
+        },
+    }, {
+        freezeTableName: true,
+        tableName: 'employee',
+        comment: "I'm a table employee!"
     })
 }
