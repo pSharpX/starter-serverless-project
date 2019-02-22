76782f99feaa47c139efc7ac85e12e3e845bc0c2
pSharpX <guardian2093@gmail.com>
2019-02-22 07:42:42 -0500

    seeder to update

---
+++ db/seeders/20190223122234-update-fake-employee.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const uuid = require("uuid/v1");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkUpdate('employee', {
+      uuid: uuid()
+    },{
+    }, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
